3206d4adef9999843d226e615bd65651262c9c9c
Vinícius Cruz <58305331+ViniciusCruzZ@users.noreply.github.com>
2023-04-19 23:21:07 -0300

    html css js starter

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-const url = 'https://pokeapi.co/api/v2/pokemon/1';
- fetch(url)
- .then(response => response.json())
- .then(data => {
-    const name = data.name;
-    const imageUrl = data.sprites.front_default;
-    const pokemonImage = document.createElement('img');
-    pokemonImage.src = imageUrl;
-    document.body.appendChild(pokemonImage);
-    const pokemonName = document.createElement('h2');
-    pokemonName.textContent = name;
-    document.body.appendChild(pokemonName);
-    console.log(data)
-}) 
-    .catch(error => {
-        console.log('Erro:', error); 
-    });
+const api = 'https://pokeapi.co/api/v2/pokemon';
+const pokeName = document.querySelector('.pokeName > p');
+const pokeImg = document.querySelector('pokeImg > img');
+const pokeInput = document.querySelector('#pokeSearch');
+const btnSearch = document.querySelector('#btnSearch')
+
+btnSearch.addEventListener('click', () => {
+    
+    fetch(api+`/${pokeInput.value}`)
+    .then(resp => resp.json())
+    .then(data => {
+        
+        const name = data.name;
+        const img = data.sprites.front_default;
+        
+        pokeName.textContent = name;
+        pokeImg.setAttribute('src', img)
+    
+        console.log(pokeName)
+    })
+    .catch(erro => {
+        alert('Erro: ' + erro)
+    })
+})
+
+
+//  .then(response => response.json())
+//  .then(data => {
+    //     const name = data.name;
+    //     const imageUrl = data.sprites.front_default;
+    //     const pokemonImage = document.createElement('img');
+    //     pokemonImage.src = imageUrl;
+    //     document.body.appendChild(pokemonImage);
+    //     const pokemonName = document.createElement('h2');
+    //     pokemonName.textContent = name;
+    //     document.body.appendChild(pokemonName);
+    //     console.log(data)
+    // })
