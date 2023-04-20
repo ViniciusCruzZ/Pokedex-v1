8afeed12f4fadf49deee37228c23e733d1f2b4ab
Vinícius Cruz <58305331+ViniciusCruzZ@users.noreply.github.com>
2023-04-20 18:10:16 -0300

    att de imagem

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
 const api = 'https://pokeapi.co/api/v2/pokemon';
 const pokeName = document.querySelector('.pokeName > p');
-const pokeImg = document.querySelector('pokeImg > img');
+const pokeImg = document.querySelector('.pokeImg > img');
 const pokeInput = document.querySelector('#pokeSearch');
 const btnSearch = document.querySelector('#btnSearch')
 
 btnSearch.addEventListener('click', () => {
     
-    fetch(api+`/${pokeInput.value}`)
-    .then(resp => resp.json())
-    .then(data => {
-        
-        const name = data.name;
-        const img = data.sprites.front_default;
-        
-        pokeName.textContent = name;
-        pokeImg.setAttribute('src', img)
+    if (pokeInput.value <= 0 || pokeInput.value > 1281) {
+        alert('Infelizmente não temos todos os pokémons, porfavor pesquise pokemons entre 1 e 1281 👍')
+    } else {
+        fetch(api+`/${pokeInput.value}`)
+        .then(resp => resp.json())
+        .then(data => {
+            const name = data.name;
+            const img = data.sprites.front_default;
+            const types = data.types
     
-        console.log(pokeName)
-    })
-    .catch(erro => {
-        alert('Erro: ' + erro)
-    })
+            pokeImg.src = img
+            pokeName.textContent = name;
+        
+            console.log(types)
+        })
+    
+        .catch(erro => {
+            alert(erro)
+        })
+    }
 })
-
-
-//  .then(response => response.json())
-//  .then(data => {
-    //     const name = data.name;
-    //     const imageUrl = data.sprites.front_default;
-    //     const pokemonImage = document.createElement('img');
-    //     pokemonImage.src = imageUrl;
-    //     document.body.appendChild(pokemonImage);
-    //     const pokemonName = document.createElement('h2');
-    //     pokemonName.textContent = name;
-    //     document.body.appendChild(pokemonName);
-    //     console.log(data)
-    // })
